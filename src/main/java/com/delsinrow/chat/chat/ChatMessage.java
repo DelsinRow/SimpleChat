e81bdbdfdcc0a48ca44534ec4eb13320f6a768a5
Sinaev Evgenii <arbidelsin@gmail.com>
2023-10-26 17:17:06 +0200

    Initial commit

---
+++ src/main/java/com/delsinrow/chat/chat/ChatMessage.java
@@ -0,0 +1,19 @@
+package com.delsinrow.chat.chat;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+    private String sender;
+    private MessageType messageType;
+}
---
+++ src/main/java/com/delsinrow/chat/chat/MessageType.java
@@ -0,0 +1,10 @@
+package com.delsinrow.chat.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
+
+
